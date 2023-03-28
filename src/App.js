21bb20d60033122795d53b158a1483590af46106
sachin.panchal <sachin.panchal@tatvasoft.com>
2023-03-28 20:15:22 +0530

    updated endpoints

--- src/App.js
+++ src/App.js
@@ -20,17 +20,23 @@ import CircularProgress from '@mui/material/CircularProgress';
 import LinearProgress from '@mui/material/LinearProgress';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import moment from 'moment'
+const baseUrl = "https://mern-autosearchbox-server.onrender.com/camera";
 
-const baseUrl = "https://mern-searchbox-server.onrender.com";
 
 function sleep(delay = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 }
+const THEME = createTheme({
+  typography: {
+    "fontFamily": `"Segoe UI"`
+  }
+});
 
 export default function App() {
-
   const [option, setAllOption] = useState([]);
   const [tableData, setAlltableData] = useState([]);
   const [dense, setDense] = React.useState(false);
@@ -38,14 +44,15 @@ export default function App() {
   let tableDataLoading = true;
   const initialState = [];
 
+
   const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDense(event.target.checked);
   };
 
   const handleInputChange = (e) => {
     // node API call for auto search option from database    
+    setAllOption(initialState);
     if (e.target.value.trim() === "") {
-      setAllOption(initialState);
       setAlltableData(initialState);
     }
     else {
@@ -53,16 +60,16 @@ export default function App() {
       try {
         (async () => {
           loading = true;          
-          //await sleep(200); // For demo purposes.         
-          var resultData = await fetch(`${baseUrl}/emails/camera/search?text=${e.target.value}`).then(res => res.json());
+          //await sleep(10000); // For demo purposes.         
+          var resultData = await fetch(`${baseUrl}/search?text=${e.target.value}`).then(res => res.json());
           setAllOption(resultData);
-          loading = false;
         })();
       }
       catch (error) {
         // TypeError: Failed to fetch
         console.log('There was an error', error);
-        loading = false;
+      } finally {
+        loading = false;          
       }
     }
   }
@@ -72,13 +79,27 @@ export default function App() {
       try {
         (async () => {
           tableDataLoading = true;
-          var url = `${baseUrl}/emails/camera/search?text=${option.SeqNo}`;
-          console.log(url);
-          await fetch(url).then(res => res.json())
+
+          var url = `${baseUrl}/topic`;
+          // await fetch(url).then(res => res.json())
+          //   .then(data => {
+          //     setAlltableData(data);
+          //   });
+
+          //body
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ topic: option.EventTopic })
+          };
+
+          await fetch(url, requestOptions)
+            .then(response => response.json())
             .then(data => {
               setAlltableData(data);
             });
-            tableDataLoading = false;
+
+          tableDataLoading = false;
         })();
       }
       catch (error) {
@@ -92,114 +113,124 @@ export default function App() {
     }
   }
 
+  const renderInput = (params) => {
+    return (<TextField {...params} label="Camera Meta-Data"
+      InputProps={{
+        ...params.InputProps,
+        startAdornment: (
+          <InputAdornment position="start"><SearchIcon style={{ cursor: "pointer" }} />
+          </InputAdornment>
+        ),
+        // this is for progress
+        endAdornment: (
+          <React.Fragment>
+            {loading ? <CircularProgress color="inherit" size={20} /> : null}
+            {params.InputProps.endAdornment}
+          </React.Fragment>
+        )
+      }}
+
+      onChange={(e) => handleInputChange(e)} />)
+  }
+
   return (
-    <div className="App">
-      <div className="divTitle">
-        <div class='inline-block-child'>
-          <img src="/favicon.ico" alt="Salient logo" style={{ width: '50x', height: "50px" }} />
+    <ThemeProvider theme={THEME}>
+      <div className="App">
+        <div className="divTitle">
+          <div className='inline-block-child'>
+            <img src="/favicon.ico" alt="Salient logo" style={{ width: '50x', height: "50px" }} />
+          </div>
+
+          <div className='inline-block-child'>
+            <strong className='SalientText'>S A L I E N T</strong>
+          </div>
+
+          <div style={{ paddingTop: "10px", margin: "auto", width: "75%" }} >
+            <p><b>Auto Searchbox &#40;PoC&#41; </b></p>
+          </div>
         </div>
 
-        <div class='inline-block-child'>
-          <strong class='SalientText'>S A L I E N T</strong>
-        </div>
-        <div style={{ padding: "10px" }} >
-          <p><b>Auto Searchbox &#40;PoC&#41; </b></p>
+        <div style={{ margin: "20px" }} >
+          <div className="inline-block-child-center">
+            <Autocomplete
+              disablePortal
+              freeSolo
+              id="combo-box-demo"
+              options={option}
+              filterOptions={(option) => option}
+              getOptionLabel={(option) => option.EventTopic}
+              loading={loading}
+              popupIcon={<SearchIcon style={{ cursor: "pointer" }} />}
+              onChange={(event, value) => handleValueChange(event, value)}
+              renderOption={(props, option) => (
+                <Card {...props} sx={{ minWidth: 275 }} key={`${option._id}`} style={{ padding: "0px", border: "none", boxShadow: "initial" }}>
+                  <CardContent align="Left" style={{ padding: "10px", borderBottom: "2px" }}>
+                    <div className="left">
+                      <Typography variant="h8">
+                        <span> {'Category :'} {option.Category}</span>
+                      </Typography>
+                    </div>
+
+                    <div className="right">
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        <span> {'Sequence No :'} {option.SeqNo}</span>
+                      </Typography>
+                    </div>
+
+                    <Typography variant="body2" >
+                      <span> {'Event Topic :'} {option.EventTopic}</span>
+                    </Typography>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                      <span> {'TimeStamp :'} {new Date(option.TimeStamp).toUTCString()}</span>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              )}
+
+              renderInput={(params) => renderInput(params)}
+
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  // Prevent's default 'Enter' behavior.
+                  event.defaultMuiPrevented = true;
+                }
+              }}
+            />
+          </div>
+
+          <FormControlLabel
+            style={{ float: "right", fontSize: "" }}
+            control={<Switch checked={dense} onChange={handleChangeDense} />}
+            label="Dense padding"
+          />
+          <TableContainer component={Paper} >
+            <Table sx={{ minWidth: 650 }} size={dense ? 'small' : 'medium'} aria-label="meta-data table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Seq&nbsp;No</TableCell>
+                  <TableCell align="left">Category</TableCell>
+                  <TableCell align="left">TimeStamp</TableCell>
+                  <TableCell align="left">Event&nbsp;Topic</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableData.map((element, row) => (
+                  <TableRow
+                    key={element._id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell align="left">{element.SeqNo}</TableCell>
+                    <TableCell align="left">{element.Category}</TableCell>
+                    <TableCell align="left">{new Date(element.TimeStamp).toUTCString()}</TableCell>
+                    <TableCell align="left">{element.EventTopic}</TableCell>
+                  </TableRow>
+                ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {/* {tableDataLoading ? <LinearProgress/> : null}           */}
         </div>
       </div>
-
-      <div style={{ margin: "20px 0px" }} >
-        <Autocomplete
-          disablePortal
-          freeSolo
-          id="combo-box-demo"
-          options={option}
-          filterOptions={(option) => option}
-          getOptionLabel={(option) => option.EventTopic}
-          sx={{ width: 500 }}
-          loading={loading}
-          popupIcon={<SearchIcon style={{ cursor: "pointer" }} />}
-          onChange={(event, value) => handleValueChange(event, value)}
-          renderOption={(props, option) => (
-            <Card {...props} sx={{ minWidth: 275 }} style={{ padding: "0px", border: "none", boxShadow: "initial" }}>
-              <CardContent align="Left" style={{ padding: "10px", borderBottom: "2px" }}>
-                <div class="left">
-                  <Typography variant="h8">
-                    <span> {'Category :'} {option.Category}</span>
-                  </Typography>
-                </div>
-
-                <div class="right">
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    <span> {'Sequence No :'} {option.SeqNo}</span>
-                  </Typography>
-                </div>
-
-                <Typography variant="body2" >
-                  <span> {'Event Topic :'} {option.EventTopic}</span>
-                </Typography>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                  <span> {'TimeStamp :'} {option.TimeStamp}</span>
-                </Typography>
-              </CardContent>
-            </Card>
-          )}
-
-          renderInput={(params) => <TextField {...params} label="Camera Meta-Data"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment: (
-                <InputAdornment position="start"><SearchIcon style={{ cursor: "pointer" }} />
-                </InputAdornment>
-              ),
-              // this is for progress
-              endAdornment: (
-                <React.Fragment>
-                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              )
-            }}
-
-            onChange={(e) => handleInputChange(e)} />}
-
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              // Prevent's default 'Enter' behavior.
-              event.defaultMuiPrevented = true;
-            }
-          }}
-        />
-      </div>
-      <FormControlLabel
-        style={{ float: "right", fontSize: "" }}
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-      <TableContainer component={Paper} >
-        <Table sx={{ minWidth: 650 }} size={dense ? 'small' : 'medium'} aria-label="meta-data table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Seq&nbsp;No</TableCell>
-              <TableCell align="left">Category</TableCell>
-              <TableCell align="left">TimeStamp</TableCell>
-              <TableCell align="left">Event&nbsp;Topic</TableCell>              
-            </TableRow>
-          </TableHead>          
-          <TableBody>           
-            {tableData.map((element, row) => (
-              <TableRow
-                key={row._Id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell align="left">{element.SeqNo}</TableCell>
-                <TableCell align="left">{element.Category}</TableCell>
-                <TableCell align="left">{element.TimeStamp}</TableCell>
-                <TableCell align="left">{element.EventTopic}</TableCell>                
-              </TableRow>
-            ))
-            }             
-          </TableBody>
-        </Table>
-      </TableContainer>                 
-    </div>
+    </ThemeProvider>
   );
 }
